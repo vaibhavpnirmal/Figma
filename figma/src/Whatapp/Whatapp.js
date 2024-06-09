e3839eff2a6d7a40fd1db2686ef874389d29a3fe
UMESH SURANGE <138711278+umeshsurnage@users.noreply.github.com>
2024-06-09 16:22:31 +0530

    in feature 1278 vaibhav nirmal is added

--- figma/src/Whatapp/Whatapp.js
+++ figma/src/Whatapp/Whatapp.js
@@ -8,7 +8,7 @@ const Whatapp = () => {
         <img src='whatapplogo.png'></img>
       </div>
       <div className='whatapptext'>
-        <h1>Umesh Surange</h1>
+        <h1>Vaibhav Nirmal</h1>
         <p>Whatsapp</p>
         <div className='screens'>
           <p>21 screens</p>
